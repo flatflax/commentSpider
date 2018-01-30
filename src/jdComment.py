9213bc81a2d9f6e2a74777abe442544bf082d6bf
flatflax <present_xm@qq.com>
2018-01-31 01:07:03 +0800

    2018/1/31
    添加不加载图片的设置，添加多线程

--- src/jdComment.py
+++ src/jdComment.py
@@ -1,38 +1,33 @@
 import unittest
-from selenium import webdriver
-import time
-from xml.dom.minidom import Document
-import xml.dom.minidom
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-from lxml import etree
 import os
+import time
+import threading
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
 
-dataPath = os.path.abspath('..')    # 工程根目录
-link = dataPath + r'\data\jdGood.txt'
-comment = dataPath + r'\data\jdComment.txt'
 
+class commentSearch:
+    def setUp(self, threadname, threadid):
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+        driver = webdriver.Chrome(chrome_options=chrome_options)
 
-class commentSearch(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.test_search_in_python_org(threadname, threadid, driver)
 
-    def test_search_in_python_org(self):
-        driver = self.driver
-        with open(link, encoding='utf-8') as reader:
-            names = []
-            links = []
-            for index, line in enumerate(reader):
-                if index % 2 == 0:
-                    names.append(line)
-                else:
-                    links.append(line)
-            reader.close()
-        length = len(names)
+    def test_search_in_python_org(self, threadname, threadid, driver):
+        if threadid == 1:
+            goodname = names[0:N//2]
+            goodlink = links[0:N//2]
+        elif threadid == 2:
+            goodname = names[N//2+1:N]
+            goodlink = links[N//2+1:N]
+
+        length = len(goodname)
         for l in range(length):
-            name = names[l]
-            url = links[l]
-            print(url)
+            name = goodname[l]
+            url = goodlink[l]
+            print(threadname, url)
 
             driver.get(url)
             time.sleep(3)
@@ -43,29 +38,25 @@ class commentSearch(unittest.TestCase):
             time.sleep(3)
 
             driver.find_element_by_xpath('//*[@id="comment"]/div[2]/div[2]/div[1]/ul/li[4]/a').send_keys(Keys.ENTER)
-            time.sleep(5)
+            time.sleep(2)
             greatid = '//div[@id="comment-3"]'
-            commentSearch.commSpider(self, greatid, name, driver)
+            commentSearch.commSpider(self, greatid, name, driver, threadname)
 
             driver.find_element_by_xpath('//*[@id="comment"]/div[2]/div[2]/div[1]/ul/li[5]/a').send_keys(Keys.ENTER)
-            time.sleep(5)
+            time.sleep(2)
             middleid = '//div[@id="comment-4"]'
-            commentSearch.commSpider(self, middleid, name, driver)
+            commentSearch.commSpider(self, middleid, name, driver, threadname)
 
             driver.find_element_by_xpath('//*[@id="comment"]/div[2]/div[2]/div[1]/ul/li[6]/a').send_keys(Keys.ENTER)
-            time.sleep(5)
+            time.sleep(2)
             badid = '//div[@id="comment-5"]'
-            commentSearch.commSpider(self, badid, name, driver)
-
-
-
-    def tearDown(self):
+            commentSearch.commSpider(self, badid, name, driver, threadname)
         self.driver.close()
 
-    def commSpider(self, idpath, name, driver):
+    def commSpider(self, idpath, name, driver, threadname):
         with open(comment, 'a', encoding='utf-8') as f:
             for i in range(300):
-                string = 'the ' + str(i) + 'th page comment'
+                string = threadname + ':the ' + str(i+1) + 'th page comment'
                 print(string)
 
                 sxpath = idpath + '//div[@class="comment-column J-comment-column"]/div[1]'
@@ -89,17 +80,49 @@ class commentSearch(unittest.TestCase):
                 try:
                     elem = driver.find_element_by_xpath(idpath +
                                                         '//div[@class="com-table-footer"]/div[1]/div[1]/a[@class="ui-pager-next"]')
-                    time.sleep(5)
+                    time.sleep(1)
                     elem.send_keys(Keys.ENTER)
                 except:
                     break
-                    str2 = str(i) + "th page has been caught."
+                    str2 = threadname + str(i+1) + "th page has been caught."
                     print(str2)
                 finally:
-                    time.sleep(5)
+                    time.sleep(1)
                     stars.clear()
                     comments.clear()
                     levels.clear()
                     times.clear()
             print("file write over")
             f.close()
+
+
+class myThread(threading.Thread):   # 继承threading.Thread
+    def __init__(self, threadid, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadid
+        self.name = name
+
+    def run(self):
+        driver = commentSearch()
+        driver.setUp(self.name, self.threadID)
+
+dataPath = os.path.abspath('..')    # 工程根目录
+link = dataPath + r'\data\jdGood.txt'
+comment = dataPath + r'\data\jdComment.txt'
+with open(link, encoding='utf-8') as reader:
+        names = []
+        links = []
+        for index, line in enumerate(reader):
+            if index % 2 == 0:
+                names.append(line)
+            else:
+                links.append(line)
+        reader.close()
+print("file load over.")
+N = len(names)
+
+thread1 = myThread(1, "Thread-1")
+thread2 = myThread(2, "Thread-2")
+
+thread1.start()
+thread2.start()

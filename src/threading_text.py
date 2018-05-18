14c2e330128028d10840cfd888c11ecae404706d
flatflax <present_xm@qq.com>
2018-05-18 22:48:00 +0800

    2018/5/18
    修改文档
    修改线程文件

--- src/threading_text.py
+++ src/threading_text.py
@@ -1,7 +1,8 @@
 # -*- coding: UTF-8 -*- 
 import threading
-import queue,time
+import queue,time,json
 import random
+from garticle_scrapy import GcoresScrap 
 
 class runner(threading.Thread):
 	def __init__(self, config):
@@ -12,9 +13,37 @@ class runner(threading.Thread):
 
 	def run(self):
 		while not self.thread_stop:
-			print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), self.name, ":new queue_id", self.q.get())
-			t = random.uniform(0,2)
-			time.sleep(t)
+			article_id = self.q.get()
+			print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), self.name, ":new queue_id", article_id)
+
+			hfile = '../result/html/article_' + str(article_id) + '.html'
+			clear_file = '../result/clear.json'
+
+			scrap_test = GcoresScrap()
+			r = scrap_test.articleScrap(article_id, hfile)
+
+			if r == True:
+				# get content of the web
+				result = scrap_test.articleClear(article_id, hfile)
+
+				# write into file with json
+				with open(clear_file, 'a+', encoding='UTF-8') as f:
+					# ensure_ascii=False : story will not be writen using unicode
+					f.write(json.dumps(result, ensure_ascii=False))
+					f.write("\n")
+					f.close()
+
+			if r == False:
+				result = scrap_test.newResultDict()
+				result["exist"] = 1
+				result["article_id"] = article_id
+
+				# write into file with json
+				with open(clear_file, 'a+', encoding='UTF-8') as f:
+					f.write(json.dumps(result, ensure_ascii=False))
+					f.write("\n")
+					f.close()
+
 
 	def stop(self):
 		self.thread_stop = True
@@ -28,7 +57,7 @@ def test():
 	config["quque"] = q
 
 	# generate a order list, use it to generate a random list
-	list_random = random.sample(range(1,1000), 100)
+	list_random = random.sample(range(1,50), 49)
 
 	# put into queue
 	for i in list_random :
